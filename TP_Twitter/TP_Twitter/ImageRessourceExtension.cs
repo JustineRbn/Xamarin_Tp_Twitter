20ae07202a4256e7ebc5e04a306e5de1d9cf8f88
Justine <justine.rbn@gmail.com>
2020-07-01 13:19:34 +0200

    modif imgbutton titlebar ok

---
+++ TP_Twitter/TP_Twitter/ImageRessourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP_Twitter
+{
+    [ContentProperty (nameof(Source))]
+    class ImageRessourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageRessourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
