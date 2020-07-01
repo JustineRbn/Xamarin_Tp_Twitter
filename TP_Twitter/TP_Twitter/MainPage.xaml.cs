75184ed0481d9e309be457f922c9857524b89d7a
Justine <justine.rbn@gmail.com>
2020-07-01 18:20:17 +0200

    add cnx btn, controller control and logic

--- TP_Twitter/TP_Twitter/MainPage.xaml.cs
+++ TP_Twitter/TP_Twitter/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -17,5 +18,44 @@ namespace TP_Twitter
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connexion demandée par l'utilisateur");
+            this.HideError();
+            String usernameStr = this.username.Text;
+            String pwdStr = this.pwd.Text;
+            Boolean isRemember = this.rememberme.IsToggled;
+
+            if(usernameStr == null || string.IsNullOrEmpty(usernameStr) || usernameStr.Length < 3)
+            {
+                this.DisplayError("Identifiant invalide. 3 caractères minimum");
+                return;
+            }
+
+            if (pwdStr == null || string.IsNullOrEmpty(pwdStr) || pwdStr.Length < 5)
+            {
+                this.DisplayError("Mot de passe invalide. Entre 5 et 10 caractères minimum");
+                return;
+            }
+            if (isRemember == true)
+            {
+                DisplayAlert("Information", "Vos informations de connexion seront enregistrées localement", "Ok");
+            }
+
+            this.ConnexionForm.IsVisible = false;
+            this.TweetList.IsVisible = true;
+        }
+        private void HideError() 
+        {
+            this.error.IsVisible = false;
+        }
+
+        private void DisplayError(string msg)
+        {
+            this.error.IsVisible = true;
+            this.error.Text = msg;
+        }
+
     }
 }
